b144d87992c4a30b5ef35f9c08ea51e7270eb52f
Ryan Kabir <rkabir@alum.mit.edu>
2011-11-25 22:00:18 -0400

    add [un]answered

--- lib/gmail/version.rb
+++ lib/gmail/version.rb
@@ -2,11 +2,11 @@ module Gmail
   class Version #:nodoc:
     MAJOR  = 0
     MINOR  = 4
-    PATCH  = 0
+    PATCH  = 1
     STRING = [MAJOR, MINOR, PATCH].join('.')
   end # Version
-  
+
   def self.version # :nodoc:
     Version::STRING
-  end 
+  end
 end # Gmail
